2dd5be3aee9ec1a5b532708fbbe0afa73a1da103
Sean Shriver <seanshi@amazon.com>
2024-03-29 19:41:02 -0500

    Cleanup old CFN template

--- .github/scripts/build-assets.py
+++ .github/scripts/build-assets.py
@@ -26,7 +26,6 @@ shutil.rmtree(os.path.join(dest_root,'assets'), ignore_errors=True)
 Path(dest_root, 'assets').mkdir(parents=True, exist_ok=False)
 data_files = ['design-patterns/cloudformation/lab.yaml',
     'design-patterns/cloudformation/C9.yaml',
-    'design-patterns/cloudformation/UserData.sh',
     'design-patterns/cloudformation/UserDataC9.sh',
     'event-driven/event-driven-cfn.yaml',
     'static/files/hands-on-labs/migration-env-setup.yaml',
